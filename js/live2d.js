11ede4ea64c4ecbc56e26b2890fa96696545c6ff
hexiangshuo <812772670@qq.com>
2026-07-13 13:44:49 +0800

    Site updated: 2026-07-13 13:44:48

--- js/live2d.js
+++ js/live2d.js
@@ -56,12 +56,13 @@ function loadExternalResource(url, type) {
   // 配置选项的具体用法见 README.md
   initWidget({
     waifuPath: live2d_path + 'waifu-tips.json',
-    // cdnPath: 'https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/',
+    cdnPath: 'https://npm.elemecdn.com/akilar-live2dapi@latest/',
     cubism2Path: live2d_path + 'live2d.min.js',
     cubism5Path: 'https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js',
-    tools: ['hitokoto', 'asteroids', 'switch-model', 'switch-texture', 'photo', 'info', 'quit'],
+    tools: ['asteroids', 'switch-model', 'switch-texture', 'photo', 'info', 'quit'],
     logLevel: 'warn',
     drag: false,
+    modelId: 0,
   });
 })();
 
